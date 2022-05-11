eeb633649ef0526c5952d44c3d1b93416695627e
Clauria Techwei <ctechwei@gmail.com>
2022-05-11 03:33:44 -0400

    Hello World in Netbeans

---
+++ HelloWorld/src/main/java/com/clauriat/helloworld/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clauriat.helloworld;
+
+/**
+ *
+ * @author Clauria Techwei
+ * email: ctechwei@gmail.com
+ * date: 05/11/2022
+ * purpose: My first Hello World program
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Clauria!");
+    }
+}
